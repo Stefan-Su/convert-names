ab0be88f7777c7579deee0e8cafa20a2b473f46c
Stefan Suter <67780957+Stefan-Su@users.noreply.github.com>
2023-04-19 12:22:57 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    dateiname = 'namen.txt'
+
+    with open(dateiname, 'r', encoding='utf-8') as datei:
+        zeilen = datei.readlines()
+
+    formatierte_namen = []
+
+    for zeile in zeilen:
+        nachname, vorname = zeile.strip().split(',')
+        formatierte_namen.append(f'{vorname.strip()} {nachname.strip()}')
+
+    ausgabe = ', '.join(formatierte_namen)
+    print(ausgabe)
+
+if __name__ == '__main__':
+    main()
